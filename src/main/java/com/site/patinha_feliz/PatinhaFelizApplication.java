c7829613fa70b4ccfc4a0fc30ec314c02dd024ea
jurineide <jurineidesouza@gmail.com>
2025-08-16 12:23:05 -0300

    inicio

---
+++ src/main/java/com/site/patinha_feliz/PatinhaFelizApplication.java
@@ -0,0 +1,13 @@
+package com.site.patinha_feliz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatinhaFelizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatinhaFelizApplication.class, args);
+	}
+
+}
